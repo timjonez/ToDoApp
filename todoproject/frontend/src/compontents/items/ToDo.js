af8edfe0f0f81972fa8f7785085845f9133bff81
Tim Jones <timjonez@protonmail.com>
2020-05-26 14:22:04 -0400

    create todo items comp

---
+++ todoproject/frontend/src/compontents/items/ToDo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ToDo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is the todo items comp</h1>
+            </div>
+        )
+    }
+}
+
+export default ToDo
