246a8f93c0148f57c8390cd83cd3547e217ded7e
Tim Jones <timjonez@protonmail.com>
2020-05-26 17:05:11 -0400

    create todo actions

---
+++ todoproject/frontend/src/actions/todo.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+import { GET_TODO } from './types';
+
+
+export const getToDo = () => dispatch => {
+    axios.get('/api/itemsl/')
+        .then(res => {
+            dispatch({
+                type: GET_TODO,
+                payload: res.data
+            });
+        })
+        .catch(err => console.log(err));
+}
