4ee0bee8bddeeacd880ce339ba3a476a9072b816
Tim Jones <timjonez@protonmail.com>
2020-05-26 14:27:08 -0400

    add Header, Dashboard to App

--- todoproject/frontend/src/compontents/App.js
+++ todoproject/frontend/src/compontents/App.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 
 import Header from './layout/Header';
+import Doshboard from './items/Dashboard';
+import Dashboard from './items/Dashboard';
 
 class App extends Component {
     render() {
-        return <Header />
+        return (
+            <Fragment>
+                <Header />
+                <div className='container'>
+                    <Dashboard />
+                </div>
+            </Fragment>
+        )
     }
 }
 
