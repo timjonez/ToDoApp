3a0ccad82d4637d301c178b98d999f230c47a59a
Tim Jones <timjonez@protonmail.com>
2020-05-26 13:14:58 -0400

    added hot-reload

--- todoproject/frontend/src/compontents/App.js
+++ todoproject/frontend/src/compontents/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import Header from './layout/Header';
+
 class App extends Component {
     render() {
-        return <h1>App Component</h1>
+        return <Header />
     }
 }
 
