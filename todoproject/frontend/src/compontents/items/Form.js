8c01b1f0fe606f7c62862d3529c43d05dd81394b
Tim Jones <timjonez@protonmail.com>
2020-05-26 14:14:59 -0400

    create form comp

---
+++ todoproject/frontend/src/compontents/items/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Form extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is the dashboard</h1>
+            </div>
+        )
+    }
+}
+
+export default Form
