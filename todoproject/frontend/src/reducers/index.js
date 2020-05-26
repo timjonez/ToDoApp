40d8f5c340f46b0f26b826d84db90417c69db37e
Tim Jones <timjonez@protonmail.com>
2020-05-26 16:58:28 -0400

    update main and index files

--- todoproject/frontend/src/reducers/index.js
+++ todoproject/frontend/src/reducers/index.js
@@ -1,3 +1,6 @@
 import { combineReducers } from "redux";
+import ToDo from './todo';
 
-export default combineReducers({});
+export default combineReducers({
+    ToDo,
+});
--- todoproject/frontend/static/frontend/main.js
+++ todoproject/frontend/static/frontend/main.js
@@ -659,6 +659,18 @@ eval("module.exports = function(originalModule) {\n\tif (!originalModule.webpack
 
 /***/ }),
 
+/***/ "./todoproject/frontend/src/actions/types.js":
+/*!***************************************************!*\
+  !*** ./todoproject/frontend/src/actions/types.js ***!
+  \***************************************************/
+/*! exports provided: GET_TODO */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"GET_TODO\", function() { return GET_TODO; });\nvar GET_TODO = 'GET_TODO';\n\n//# sourceURL=webpack:///./todoproject/frontend/src/actions/types.js?");
+
+/***/ }),
+
 /***/ "./todoproject/frontend/src/compontents/App.js":
 /*!*****************************************************!*\
   !*** ./todoproject/frontend/src/compontents/App.js ***!
@@ -739,7 +751,19 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _com
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var redux__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! redux */ \"./node_modules/redux/es/redux.js\");\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Object(redux__WEBPACK_IMPORTED_MODULE_0__[\"combineReducers\"])({}));\n\n//# sourceURL=webpack:///./todoproject/frontend/src/reducers/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var redux__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! redux */ \"./node_modules/redux/es/redux.js\");\n/* harmony import */ var _todo__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./todo */ \"./todoproject/frontend/src/reducers/todo.js\");\n\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Object(redux__WEBPACK_IMPORTED_MODULE_0__[\"combineReducers\"])({\n  ToDo: _todo__WEBPACK_IMPORTED_MODULE_1__[\"default\"]\n}));\n\n//# sourceURL=webpack:///./todoproject/frontend/src/reducers/index.js?");
+
+/***/ }),
+
+/***/ "./todoproject/frontend/src/reducers/todo.js":
+/*!***************************************************!*\
+  !*** ./todoproject/frontend/src/reducers/todo.js ***!
+  \***************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _actions_types_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../actions/types.js */ \"./todoproject/frontend/src/actions/types.js\");\nfunction ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }\n\nfunction _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }\n\nfunction _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }\n\n\nvar initialState = {\n  todo: []\n};\n/* harmony default export */ __webpack_exports__[\"default\"] = (function () {\n  var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : initialState;\n  var action = arguments.length > 1 ? arguments[1] : undefined;\n\n  switch (action.type) {\n    case _actions_types_js__WEBPACK_IMPORTED_MODULE_0__[\"GET_TODO\"]:\n      return _objectSpread(_objectSpread({}, state), {}, {\n        todo: action.payload\n      });\n\n    default:\n      return state;\n  }\n});\n\n//# sourceURL=webpack:///./todoproject/frontend/src/reducers/todo.js?");
 
 /***/ }),
 
