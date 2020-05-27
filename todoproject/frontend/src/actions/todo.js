924ed006dae731ce842e2a7ae5352e08d4b2a325
Tim Jones <timjonez@protonmail.com>
2020-05-27 19:54:37 -0400

    correct message for delete todo

--- todoproject/frontend/src/actions/todo.js
+++ todoproject/frontend/src/actions/todo.js
@@ -18,7 +18,7 @@ export const getToDo = () => dispatch => {
 export const deleteToDo = (id) => dispatch => {
     axios.delete(`api/items/${id}`)
         .then(res => {
-            dispatch(createMessage({ deleteToDo: 'Lead Added' }));
+            dispatch(createMessage({ deleteToDo: 'To Do Deleted' }));
             dispatch({
                 type: DELETE_TODO,
                 payload: id
