411e7c8a202b3fd22c92a504777b240f6227e96f
Tim Jones <timjonez@protonmail.com>
2020-05-27 09:41:27 -0400

    fix bug in Alerts comp

--- todoproject/frontend/src/compontents/layout/Alerts.js
+++ todoproject/frontend/src/compontents/layout/Alerts.js
@@ -1,25 +1,33 @@
 import React, { Component, Fragment } from 'react';
 import { withAlert } from 'react-alert';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
+
+
 
 export class Alerts extends Component {
     static propTypes = {
-        error: PropTypes.object.isRequired
+        error: PropTypes.object.isRequired,
+        message: PropTypes.object.isRequired,
     }
 
-    componentDidMount(prevProps) {
-        const { error, alert } = this.props;
-        if (error !== prevProps.error) { alert.error('this works') };
-    }
+    componentDidUpdate(prevProps) {
+        const { error, alert, message } = this.props
+        if (error !== prevProps.error) {
+            if (error.msg.title) alert.error(`Title: ${error.msg.title.join()}`);
+            if (error.msg.body) alert.error(`Body: ${error.msg.body.join()}`);
+            if (error.msg.non_field_errors) alert.error(error.msg.non_field_errors.join());
+        }
 
+    }
     render() {
-        return <Fragment />;
+        return <Fragment />
     }
 }
 
 const mapStateToProps = state => ({
-    error: state.errors
-})
+    error: state.errors,
+    message: state.messages
+});
 
 export default connect(mapStateToProps)(withAlert()(Alerts));
