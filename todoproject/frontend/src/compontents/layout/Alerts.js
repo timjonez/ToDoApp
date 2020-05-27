23073b053dd48db026e18a2e9c512cbba50ffd33
Tim Jones <timjonez@protonmail.com>
2020-05-27 09:44:44 -0400

    add error for due date

--- todoproject/frontend/src/compontents/layout/Alerts.js
+++ todoproject/frontend/src/compontents/layout/Alerts.js
@@ -16,6 +16,7 @@ export class Alerts extends Component {
         if (error !== prevProps.error) {
             if (error.msg.title) alert.error(`Title: ${error.msg.title.join()}`);
             if (error.msg.body) alert.error(`Body: ${error.msg.body.join()}`);
+            if (error.msg.due_date) alert.error(`Body: ${error.msg.due_date.join()}`);
             if (error.msg.non_field_errors) alert.error(error.msg.non_field_errors.join());
         }
 
