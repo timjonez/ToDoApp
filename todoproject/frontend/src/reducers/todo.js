176532fa5d0432d8056c7b02d168561a33b50911
Tim Jones <timjonez@protonmail.com>
2020-05-26 21:46:34 -0400

    add reducer for add todo

--- todoproject/frontend/src/reducers/todo.js
+++ todoproject/frontend/src/reducers/todo.js
@@ -1,4 +1,4 @@
-import { GET_TODO, DELETE_TODO } from '../actions/types.js';
+import { GET_TODO, DELETE_TODO, ADD_TODO } from '../actions/types.js';
 
 const initialState = {
     todo: []
@@ -15,6 +15,11 @@ export default function (state = initialState, action) {
             return {
                 ...state,
                 todo: state.todo.filter(todo => todo.id !== action.payload)
+            }
+        case ADD_TODO:
+            return {
+                ...state,
+                todo: [...state.todo, action.payload]
             }
         default:
             return state;
