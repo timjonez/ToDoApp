475e2bd25ff23a17bd079f45dd0e2a882777bddf
Tim Jones <timjonez@protonmail.com>
2020-05-26 22:18:08 -0400

    create alerts comp

---
+++ todoproject/frontend/src/compontents/layout/Alerts.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+import { withAlert } from 'react-alert';
+
+export class Alerts extends Component {
+    componentDidMount() {
+    }
+
+    render() {
+        return <Fragment />;
+    }
+}
+
+export default withAlert(Alerts);
