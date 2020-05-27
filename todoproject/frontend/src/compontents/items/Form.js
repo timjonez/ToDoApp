ef3425836b222ba835aa1dc27b2e6c20fe494494
Tim Jones <timjonez@protonmail.com>
2020-05-26 21:14:24 -0400

    added form to Form comp

--- todoproject/frontend/src/compontents/items/Form.js
+++ todoproject/frontend/src/compontents/items/Form.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react'
 
 export class Form extends Component {
+    state = {
+        title: '',
+        body: '',
+        due_date: ''
+    };
+
+    onChange = e => this.setState({ [e.target.name]: e.target.value });
+
     render() {
+        const { title, body, due_date } = this.state;
         return (
-            <div>
-                <h1>This is the dashboard</h1>
+            <div className='card card-body mt-4 mb-4'>
+                <h2>Add To Do Item</h2>
+                <form onSubmit={this.onSubmit}>
+                    <div className='form-group'>
+                        <label>Title</label>
+                        <input
+                            className='form-control'
+                            type='text'
+                            onChange={this.onChange}
+                            value={title} />
+                    </div>
+                    <div className='form-group'>
+                        <label>To Do</label>
+                        <input
+                            className='form-control'
+                            type='text'
+                            onChange={this.onChange}
+                            value={body} />
+                    </div>
+                    <div className='form-group'>
+                        <label>Due On</label>
+                        <input
+                            className='form-control'
+                            type='datetime-local'
+                            onChange={this.onChange}
+                            value={due_date} />
+                    </div>
+                </form>
             </div>
         )
     }
