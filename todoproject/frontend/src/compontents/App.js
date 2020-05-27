7e3fa101f7552a4a1d3269058cd2692237b1c616
Tim Jones <timjonez@protonmail.com>
2020-05-26 22:13:59 -0400

    create alert provider

--- todoproject/frontend/src/compontents/App.js
+++ todoproject/frontend/src/compontents/App.js
@@ -4,19 +4,29 @@ import ReactDOM from 'react-dom';
 import Header from './layout/Header';
 import Dashboard from './items/Dashboard';
 
+import { Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+
 import { Provider } from 'react-redux';
 import store from '../store'
+
+const alertOptions = {
+    timeout: 3000,
+    position: 'top center'
+}
 
 class App extends Component {
     render() {
         return (
             <Provider store={store}>
-                <Fragment>
-                    <Header />
-                    <div className='container'>
-                        <Dashboard />
-                    </div>
-                </Fragment>
+                <AlertProvider template={AlertTemplate} {...alertOptions}>
+                    <Fragment>
+                        <Header />
+                        <div className='container'>
+                            <Dashboard />
+                        </div>
+                    </Fragment>
+                </AlertProvider>
             </Provider>
         )
     }
