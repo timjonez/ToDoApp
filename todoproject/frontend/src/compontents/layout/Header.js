9e452e175a61545f928bef77f749bb3bc873221e
Tim Jones <timjonez@protonmail.com>
2020-05-28 11:49:39 -0400

    add container to Header comp

--- todoproject/frontend/src/compontents/layout/Header.js
+++ todoproject/frontend/src/compontents/layout/Header.js
@@ -5,19 +5,21 @@ export class Header extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-sm navbar-light bg-light">
-                <a className="navbar-brand" href="#">Tim's ToDo</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <ul className="navbar-nav ml-auto">
-                        <li className='nav-item'>
-                            <Link to='/register' className='nav-link'>Register</Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to='/login' className='nav-link'>Login</Link>
-                        </li>
-                    </ul>
+                <div className='container'>
+                    <a className="navbar-brand" href="#">Tim's ToDo</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <ul className="navbar-nav ml-auto">
+                            <li className='nav-item'>
+                                <Link to='/register' className='nav-link'>Register</Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link to='/login' className='nav-link'>Login</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         )
