900b3662875ead5821f6fc017aeda235e555d0dc
Tim Jones <timjonez@protonmail.com>
2020-05-27 20:06:03 -0400

    add user to viewset

--- todoproject/todo_items/api.py
+++ todoproject/todo_items/api.py
@@ -3,7 +3,12 @@ from rest_framework import viewsets, permissions
 from .serializers import ItemSerializer
 
 class ItemViewSet(viewsets.ModelViewSet):
-    queryset = Item.objects.all()
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.isAuthenticated]
     serializer_class = ItemSerializer
+
+    def get_queryset(self):
+        return self.request.user.items.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
     
