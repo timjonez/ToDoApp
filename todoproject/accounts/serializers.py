a9dee984b84849ffbda32fdfe6ed5b887fae51e7
Tim Jones <timjonez@protonmail.com>
2020-05-28 09:48:19 -0400

    fix bug in serializers

--- todoproject/accounts/serializers.py
+++ todoproject/accounts/serializers.py
@@ -2,10 +2,10 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 
- class UserSerializer(serializers.ModelSerializer):
-     class Meta: 
-         model = User
-         fields = ('id','username','email')
+class UserSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = User
+        fields = ('id','username','email')
 
 
 class RegisterSerializer(serializers.ModelSerializer):
