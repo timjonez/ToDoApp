4664efbc057d300127dc8182d3d0f45a1d088a07
Tim Jones <timjonez@protonmail.com>
2020-05-28 13:02:04 -0400

    add login to auth action

--- todoproject/frontend/src/actions/auth.js
+++ todoproject/frontend/src/actions/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { returnErrors } from './messages';
 
-import { USER_LOADING, USER_LOADED, AUTH_ERROR } from './types';
+import { USER_LOADING, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL } from './types';
 
 export const loadUser = () => (dispatch, getState) => {
     dispatch({ type: USER_LOADING });
@@ -22,5 +22,24 @@ export const loadUser = () => (dispatch, getState) => {
         }).catch(err => {
             dispatch(returnErrors(err.response.data, err.response.status))
             dispatch({ type: AUTH_ERROR })
+        })
+}
+
+export const login = (username, password) => dispatch => {
+    const config = {
+        headers: { 'Content-Type': 'application/json' }
+    }
+
+    const body = JSON.stringify({ username, password });
+
+    axios.post('/api/auth/user', body, config)
+        .then(res => {
+            dispatch({
+                type: LOGIN_SUCCESS,
+                payload: res.data
+            })
+        }).catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status))
+            dispatch({ type: LOGIN_FAIL })
         })
 }
