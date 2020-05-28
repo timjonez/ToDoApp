db95b6ef1629bda20488dd43573a8f421edfb912
Tim Jones <timjonez@protonmail.com>
2020-05-28 12:14:14 -0400

    fix typos in PrivateRouter

--- todoproject/frontend/src/compontents/common/PrivateRoute.js
+++ todoproject/frontend/src/compontents/common/PrivateRoute.js
@@ -3,7 +3,7 @@ import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-const PrivateRoute = ({ component: Component, auth, ...rest }) {
+const PrivateRoute = ({ component: Component, auth, ...rest }) =>
     <Route
         {...rest}
         render={props => {
@@ -15,8 +15,8 @@ const PrivateRoute = ({ component: Component, auth, ...rest }) {
                 return <Component {...props} />
             }
         }}
-    />;
-}
+    />
+
 
 const mapStateToProps = state => ({
     auth: state.auth
