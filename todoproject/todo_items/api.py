ad6fcba677c996026c003ffedd593272a775d592
Tim Jones <timjonez@protonmail.com>
2020-05-27 20:07:28 -0400

    fix bug typo

--- todoproject/todo_items/api.py
+++ todoproject/todo_items/api.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets, permissions
 from .serializers import ItemSerializer
 
 class ItemViewSet(viewsets.ModelViewSet):
-    permission_classes = [permissions.isAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = ItemSerializer
 
     def get_queryset(self):
