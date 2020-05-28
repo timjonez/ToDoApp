e678d3a7397f390a4c9c94c8809d7541dfbb70a8
Tim Jones <timjonez@protonmail.com>
2020-05-28 14:44:12 -0400

    add logout action to Header

--- todoproject/frontend/src/compontents/layout/Header.js
+++ todoproject/frontend/src/compontents/layout/Header.js
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
 export class Header extends Component {
     static propTypes = {
-        auth: PropTypes.object.isRequired
+        auth: PropTypes.object.isRequired,
+        logout: PropTypes.func.isRequired
     }
 
     render() {
@@ -15,7 +17,9 @@ export class Header extends Component {
         const authLinks = (
             <ul className="navbar-nav ml-auto">
                 <li className='nav-item'>
-                    <button className='nav-link btn btn-info btn-sm text-light'>Logout</button>
+                    <button
+                        onClick={this.props.logout}
+                        className='nav-link btn btn-info btn-sm text-light'>Logout</button>
                 </li>
             </ul>
         )
@@ -51,4 +55,4 @@ const mapStateToProps = state => ({
     auth: state.auth
 })
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps, { logout })(Header);
