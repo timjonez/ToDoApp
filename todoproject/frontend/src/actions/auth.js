ef45a92175216d95f0f7402dc85d455c74d9bab0
Tim Jones <timjonez@protonmail.com>
2020-05-28 12:37:14 -0400

    create auth actions

---
+++ todoproject/frontend/src/actions/auth.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { returnErrors } from './messages';
+
+import { USER_LOADED, USER_LOADED, AUTH_ERROR, USER_LOADING } from './types';
+
+export const loadUser = () => (dispatch, getState) => {
+    dispatch({ type: USER_LOADING });
+
+    const token = getState().auth.token;
+    const config = {
+        headers: { 'Content-Type': 'application/json' }
+    }
+
+    if (token) { config.headers['Authorization'] = `Token ${token}` }
+
+    axios.get('/api/auth/user', config)
+        .then(res => {
+            dispatch({
+                type: USER_LOADED,
+                payload: res.data
+            })
+        }).catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status))
+            dispatch({ type: AUTH_ERROR })
+        })
+}
