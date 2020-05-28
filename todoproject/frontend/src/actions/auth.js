5b703775c27c0eddddfc368a50137caffc24bea0
Tim Jones <timjonez@protonmail.com>
2020-05-28 16:11:39 -0400

    add register action

--- todoproject/frontend/src/actions/auth.js
+++ todoproject/frontend/src/actions/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { returnErrors } from './messages';
 
-import { USER_LOADING, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT_SUCCESS } from './types';
+import { USER_LOADING, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT_SUCCESS, REGISTER_SUCCESS, REGISTER_FAIL } from './types';
 
 export const loadUser = () => (dispatch, getState) => {
     dispatch({ type: USER_LOADING });
@@ -36,6 +36,26 @@ export const login = (username, password) => dispatch => {
             dispatch({ type: LOGIN_FAIL })
         })
 }
+
+export const register = ({ username, password, email }) => dispatch => {
+    const config = {
+        headers: { 'Content-Type': 'application/json' }
+    }
+
+    const body = JSON.stringify({ username, password, email });
+
+    axios.post('/api/auth/register', body, config)
+        .then(res => {
+            dispatch({
+                type: REGISTER_SUCCESS,
+                payload: res.data
+            })
+        }).catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status))
+            dispatch({ type: REGISTER_FAIL })
+        })
+}
+
 
 export const logout = () => (dispatch, getState) => {
     axios.post('/api/auth/logout', null, tokenConfig(getState))
