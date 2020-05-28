1bdbbc3b39f771712507fe1e93d3654895a7c1f6
Tim Jones <timjonez@protonmail.com>
2020-05-28 11:46:11 -0400

    fix typo in Login

--- todoproject/frontend/src/compontents/accounts/Login.js
+++ todoproject/frontend/src/compontents/accounts/Login.js
@@ -15,7 +15,7 @@ export class Login extends Component {
     onChange = e => this.setState({ [e.target.name]: e.target.value });
 
     render() {
-        const = { username, password, } = this.state
+        const { username, password, } = this.state
         return (
             <div className='col-md-6 m-auto'>
                 <div className='card card-body mt-5'>
