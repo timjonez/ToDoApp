4be0cc13f064cf40d3d64979a9e45ef88d511b2b
Tim Jones <timjonez@protonmail.com>
2020-05-27 20:04:05 -0400

    add user to model

--- todoproject/todo_items/models.py
+++ todoproject/todo_items/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Item(models.Model):
@@ -6,3 +7,4 @@ class Item(models.Model):
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     due_date = models.DateTimeField()
+    owner = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE, null=True)
