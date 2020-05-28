347bd90e7e7ab5a850a21c633a61de42982acd11
Tim Jones <timjonez@protonmail.com>
2020-05-28 16:34:05 -0400

    add submit logic for register comp

--- todoproject/frontend/src/compontents/accounts/Register.js
+++ todoproject/frontend/src/compontents/accounts/Register.js
@@ -20,11 +20,16 @@ export class Register extends Component {
 
     onSubmit = e => {
         e.preventDefault();
-        const { password, password2 } = this.state;
+        const { username, email, password, password2 } = this.state;
         if (password !== password2) {
             this.props.createMessage({ passwordNotMatch: 'Passwords do not match' })
         } else {
-            console.log('submit');
+            const newUser = {
+                email,
+                username,
+                password
+            }
+            this.props.register(newUser);
         }
     }
 
