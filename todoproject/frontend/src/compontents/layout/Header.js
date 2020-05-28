b88d5fbd83d4c234b0135ffe8871e37ead014b6a
Tim Jones <timjonez@protonmail.com>
2020-05-28 14:33:28 -0400

    add condition rendering to Heading

--- todoproject/frontend/src/compontents/layout/Header.js
+++ todoproject/frontend/src/compontents/layout/Header.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 export class Header extends Component {
+    static propTypes = {
+        auth: PropTypes.object.isRequired
+    }
+
     render() {
+        const { isAuthenticated, user } = this.props.auth;
+
+        const authLinks = (
+            <ul className="navbar-nav ml-auto">
+                <li className='nav-item'>
+                    <button className='nav-link btn btn-info btn-sm text-light'>Logout</button>
+                </li>
+            </ul>
+        )
+
+        const guestLinks = (
+            <ul className="navbar-nav ml-auto">
+                <li className='nav-item'>
+                    <Link to='/register' className='nav-link'>Register</Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/login' className='nav-link'>Login</Link>
+                </li>
+            </ul>
+        )
+
         return (
             <nav className="navbar navbar-expand-sm navbar-light bg-light">
                 <div className='container'>
@@ -11,14 +39,7 @@ export class Header extends Component {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <ul className="navbar-nav ml-auto">
-                            <li className='nav-item'>
-                                <Link to='/register' className='nav-link'>Register</Link>
-                            </li>
-                            <li className='nav-item'>
-                                <Link to='/login' className='nav-link'>Login</Link>
-                            </li>
-                        </ul>
+                        {isAuthenticated ? authLinks : guestLinks}
                     </div>
                 </div>
             </nav>
@@ -26,4 +47,8 @@ export class Header extends Component {
     }
 }
 
-export default Header
+const mapStateToProps = state => ({
+    auth: state.auth
+})
+
+export default connect(mapStateToProps)(Header);
